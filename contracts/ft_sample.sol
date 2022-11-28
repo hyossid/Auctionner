adf35e8916981ec9f0a5575475072256d95195e0
hyossid <huvsid@gmail.com>
2022-11-28 17:26:01 +0900

    chore: add lint

--- contracts/ft_sample.sol
+++ contracts/ft_sample.sol
@@ -1,28 +1,27 @@
 pragma solidity ^0.8.0;
-import 'hardhat/console.sol';
-
+import "hardhat/console.sol";
 
 contract Token {
-    string public name = 'Sidney_Token';
-    string public symbol = 'STK';
-    uint public totalSupply = 100000;
-    address public owner;
-    mapping(address =>uint) balances;
+  string public name = "Sidney_Token";
+  string public symbol = "STK";
+  uint public totalSupply = 100000;
+  address public owner;
+  mapping(address => uint) balances;
 
-    constructor(){
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
-    }
+  constructor() {
+    balances[msg.sender] = totalSupply;
+    owner = msg.sender;
+  }
 
-    function transfer(address to, uint amount) external {
-        console.log('Sender balance is %s tokens', balances[msg.sender]);
-        require(balances[msg.sender]>=amount,'Not enough tokens');
-        
-        balances[msg.sender] -=amount;
-        balances[to] += amount;
-    }
+  function transfer(address to, uint amount) external {
+    console.log("Sender balance is %s tokens", balances[msg.sender]);
+    require(balances[msg.sender] >= amount, "Not enough tokens");
 
-    function balanceOf(address account) external view returns(uint){
-        return balances[account];
-    }
+    balances[msg.sender] -= amount;
+    balances[to] += amount;
+  }
+
+  function balanceOf(address account) external view returns (uint) {
+    return balances[account];
+  }
 }
